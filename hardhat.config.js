d49d92d57390d6a8c78363b4478a4d99c4844e75
Boyuan Qian <1044147+reddqian@users.noreply.github.com>
2021-10-22 08:20:53 +0800

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+
+const { privateKey } = require("./secrets.json");
+
+module.exports = {
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [privateKey],
+    },
+
+    polygon: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [privateKey],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "YOUR_API_KEY",
+  },
+
+  solidity: "0.8.0",
+};
